1702c606756a7bee95130d163afde0a4b79b7536
yonathang <yonathang@ui.co.il>
2021-12-23 14:23:15 +0200

    require and connect in each lambda function

--- handler.js
+++ handler.js
@@ -1,18 +1,29 @@
 'use strict';
 const utilities = require('./services/utilities')
+// const dbjs = require('./services/db.js');
+////////////////////////////////////////////////////////////
+const MongoClient = require('mongodb').MongoClient;
+// const MONGODB_URI = process.env.MONGO_CONNECTION_STR;
+const MONGODB_URI = "mongodb+srv://galiliyo:y46aq7HW53NAr3@cluster0.gbzfo.mongodb.net/ui_atlas_db?retryWrites=true&w=majority";
+let dbInstance = null;
+let db = null;
+
+const getCollection = async function (collectionName) {
+    if (db) {
+        db = Promise.resolve(dbInstance);
+    } else {
+        db = await MongoClient.connect(MONGODB_URI);
+        db = db.db("ui_db");
+    }
+    return db.collection(collectionName);
+}
+/////////////////////////////////////////////////////////////////
+
 
 module.exports.ping = async event => {
 
-    // const origin = event?.headers?.origin || event?.headers?.Origin || "http://localhost:4201";
-
-    // console.log(`getHeaders():`, utilities.getHeaders())
-
     console.log(`ping origin:`, event)
     const res = {'status': 'online'}
-    //  headers = {
-    //     'Access-Control-Allow-Origin': origin,
-    //     'Access-Control-Allow-Credentials': true,
-    // }
 
     const headers = utilities.getHeaders(event)
     return {statusCode: 200, headers, body: JSON.stringify(res)};
@@ -24,10 +35,11 @@ module.exports.setCookie = async (event, context) => {
     const additionalHeaders = {
         "Access-Control-Expose-Headers": "Set-Cookie",
         "Access-Control-Allow-Headers": "Set-Cookie",
+        "Content-Type": "text/html; charset=UTF-8",
         "Set-Cookie": "user=YonathanG; expires=Thu, 19 Apr 2022 20:41:27 GMT;  SameSite=None; Path=/; Secure",
     }
 
-    const headers = utilities.getHeaders(event,additionalHeaders)
+    const headers = utilities.getHeaders(event, additionalHeaders)
     console.log(`setCookie - headers:`, headers)
 
     return {statusCode: 200, headers, body: JSON.stringify(event)};
@@ -73,13 +85,13 @@ module.exports.setCookie = async (event, context) => {
 
 
 module.exports.login = async (event, context) => {
-    console.log(`event :`, event )
+    console.log(`event :`, event)
     const additionalHeaders = {
         "access-control-expose-headers": "Set-Cookie",
         "Set-Cookie": "user=YonathanG; expires=Thu, 19 Apr 2022 20:41:27 GMT;  SameSite=None; Secure",
     }
 
-    const headers = utilities.getHeaders(event,additionalHeaders)
+    const headers = utilities.getHeaders(event, additionalHeaders)
     console.log(`setCookie - headers:`, headers)
 
     return {statusCode: 200, headers, body: JSON.stringify(event)};
@@ -89,12 +101,31 @@ module.exports.login = async (event, context) => {
 
 
 module.exports.isLoggedIn = async (event, context) => {
+    ////////////////////////////////////////////////////////////
+    const MongoClient = require('mongodb').MongoClient;
+// const MONGODB_URI = process.env.MONGO_CONNECTION_STR;
+    const MONGODB_URI = "mongodb+srv://galiliyo:y46aq7HW53NAr3@cluster0.gbzfo.mongodb.net/ui_atlas_db?retryWrites=true&w=majority";
+    let dbInstance = null;
+    let db = null;
+
+    const getCollection = async function (collectionName) {
+        if (db) {
+            db = Promise.resolve(dbInstance);
+        } else {
+            db = await MongoClient.connect(MONGODB_URI);
+            db = db.db("ui_db");
+        }
+        return db.collection(collectionName);
+    }
+/////////////////////////////////////////////////////////////////
+
+
     context.callbackWaitsForEmptyEventLoop = false;
 
     try {
         console.log(`isloggedin event:`, event)
-        // const users = await dbjs.getCollection('users')
-        // const userFromCookie = event.headers?.cookie?.user // is the way??
+        const users = await getCollection('users')
+        const userFromCookie = event.headers?.cookie?.user // is the way??
         const headers = utilities.getHeaders(event)
         // if (!userFromCookie) {
         //     return {
@@ -104,16 +135,17 @@ module.exports.isLoggedIn = async (event, context) => {
         //     };
         // }
 
-        //   const res =  await users.findOne({'name': userFromCookie || 'HGil'}, (err, result) => {
-        //       console.log(`result:`, result)
-        //     return {statusCode: 200, body: JSON.stringify({ ...result , result: true})}
-        // })
+        const res = await users.findOne({'name': userFromCookie || 'HGil'}, (err, result) => {
+            console.log(`result:`, result)
+            return {statusCode: 200, body: JSON.stringify({...result, result: true})}
+        })
 
-        // const res =  await users.findOne({'name': userFromCookie || 'HGil'}, (err, result) => {
+        //
         // const res = await users.findOne({'name': userFromCookie || 'HGil'})
         // console.log(`isLoggedIn response:`, headers, JSON.stringify({...res, result: true}))
-        return {statusCode: 200, headers,body: JSON.stringify({result: true})}
-    } catch (err) {
+        // return {statusCode: 200, headers, body: JSON.stringify({result: true})}
+    } catch
+        (err) {
         console.log(`isLoggedIn error :`, err)
         return {statusCode: 500, body: err.message}
     }
