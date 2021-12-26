3777b3db22dadc7815685ddc08d4160ea70c5504
yonathang <yonathang@ui.co.il>
2021-12-26 11:12:57 +0200

    local cookie works

--- handler.js
+++ handler.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const mockUser = {
+    "%job": "100%",
+    bonusSteps: ["70", "80", "100"],
+    collect: "1",
+    department: "Imp",
+    email: "YonathanG@ui.co.il",
+    employee_num: "180",
+    free_lancer: "0",
+    id: "028695468",
+    level: "33 Programmer",
+    level_code: "33",
+    local_level: "3",
+    lunch_hours: "5",
+    name: "YonathanG",
+    name2: "יונתן גלילי",
+    result: true,
+    sendmail: "0",
+    _id: "61b5cebcf604f0304db1e309"
+}
+
+
 module.exports.setCookie = async (event, context) => {
 
     const origin = event?.headers?.origin;
@@ -33,4 +54,46 @@ module.exports.ping = async (event, context) => {
     return {statusCode: 200, headers, body: `app was pinged from ${origin} `};
 }
 
+module.exports.login = async (event, context) => {
+    console.log(`event :`, event)
+    const origin = event?.headers?.origin;
+    // const headers = utilities.getHeaders(event, additionalHeaders)
+    const headers = {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': true,
+    }
+
+    return {statusCode: 200, headers, body: JSON.stringify(mockUser)};
+
+}
+
+module.exports.isLoggedIn = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const origin = event?.headers?.origin;
+    try {
+        console.log(`isloggedin event:`, event)
+        // const users = await dbjs.getCollection('users')
+        // const userFromCookie = event.headers?.cookie?.user // is the way??
+        // const headers = utilities.getHeaders(event)
+        // if (!userFromCookie) {
+        //     return {
+        //         statusCode: 401, body: JSON.stringify({
+        //             result: false
+        //         })
+        //     };
+        // }
+        const headers = {
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Credentials': true,
+        }
+        // const res = await users.findOne({'name': userFromCookie || 'HGil'})
+        // console.log(`isLoggedIn response:`, headers, JSON.stringify({...res, result: true}))
+        return {statusCode: 200, headers, body: JSON.stringify({result: false})}
+
+        // return {statusCode: 200, headers, body: JSON.stringify({...mockUser, result: true})}
+    } catch (err) {
+        console.log(`isLoggedIn error :`, err)
+        return {statusCode: 500, body: err.message}
+    }
+}
 
